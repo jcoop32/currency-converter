aa67a131458544f2327a5a12a4ac6f1d5e32249b
Josh Cooper <joshcooper@matrix.lan>
2023-03-13 17:19:18 -0500

    code cleanup

--- currencyConverter.cpp
+++ currencyConverter.cpp
@@ -3,20 +3,17 @@
 #include <locale>
 #include "currencyRates.cpp"
 
-
 using namespace std;
+
+double convert(double a, double b){
+    return a * b;
+}
 
 int main(){
     Rates rate;
     double dollar;
     cout << "Enter US dollar amount: $";
     cin >> dollar;
-    double toEuro = dollar * rate.euro; double toPound = dollar * rate.gbp;
-    double toPeso = dollar * rate.peso; double toCad = dollar * rate.cad;
-    double toAud = dollar * rate.aud; double toJpy = dollar * rate.jpy;
-    double toInr = dollar * rate.inr; double toSgd = dollar * rate.sgd;
-    double toChf = dollar * rate.chf; double toCny = dollar * rate.cny;
-   
 
 	using std::setw;
 	cout << left;
@@ -24,23 +21,24 @@ int main(){
     cout.imbue(locale("en_US.UTF-8"));
 	cout <<
 	    setw(9) << "[Currency]" << "   "    << setw(6) << "[Rate]"     << "\t"  << setw(5) << "[Amount after Conversion]"        << '\n' <<
-	    setw(9) << "   Euro"       << "|"  << "    "  << setw(6) << rate.euro  << " |"  << setw(5) << "\t " << "€" << fixed << setprecision(2) << toEuro     << '\n' <<
-        setw(9) << "   GBP "       << "|"  << "    "  << setw(6) << rate.gbp   << " |"  << setw(5) << "\t " << "£"<< fixed << setprecision(2) << toPound     << '\n' <<
-        setw(9) << "   Peso"       << "|"  << "    "  << setw(6) << rate.peso  << " |"  << setw(5) << "\t " << "$"<< fixed << setprecision(2) << toPeso      << '\n' <<
-        setw(9) << "   CAD "       << "|"  << "    "  << setw(6) << rate.cad   << " |"  << setw(5) << "\t " << "$"<< fixed << setprecision(2) << toCad       << '\n' <<
-        setw(9) << "   AUD "       << "|"  << "    "  << setw(6) << rate.aud   << " |"  << setw(5) << "\t " << "$"<< fixed << setprecision(2) << toAud       << '\n' <<
-        setw(9) << "   JPY "       << "|"  << "    "  << setw(6) << rate.jpy   << " |"  << setw(5) << "\t " << "¥"<< fixed << setprecision(2) << toJpy       << '\n' <<
-        setw(9) << "   INR "       << "|"  << "    "  << setw(6) << rate.inr   << " |"  << setw(5) << "\t " << "₹"<< fixed << setprecision(2) << toInr       << '\n' <<
-        setw(9) << "   SGD "       << "|"  << "    "  << setw(6) << rate.sgd   << " |"  << setw(5) << "\t " << "$"<< fixed << setprecision(2) << toSgd       << '\n' <<
-	    setw(9) << "   CHF "       << "|"  << "    "  << setw(6) << rate.chf   << " |"  << setw(5) << "\t " << "₣"<< fixed << setprecision(2) << toChf       << '\n' <<
-	    setw(9) << "   CNY "       << "|"  << "    "  << setw(6) << rate.cny   << " |"  << setw(5) << "\t " << "¥"<< fixed << setprecision(2) << toCny       << '\n';
-    cout << "----------------------------------------------------" << endl;
+	    setw(9) << "   Euro"       << "|"  << "    "  << setw(6) << rate.euro  << " |"  << setw(5) << "\t " << "€ " << fixed << setprecision(2) << convert(dollar, rate.euro) << '\n' <<
+        setw(9) << "   GBP "       << "|"  << "    "  << setw(6) << rate.gbp   << " |"  << setw(5) << "\t " << "£ "<<  fixed << setprecision(2)  << convert(dollar, rate.gbp)  << '\n' <<
+        setw(9) << "   Peso"       << "|"  << "    "  << setw(6) << rate.peso  << " |"  << setw(5) << "\t " << "$ "<<  fixed << setprecision(2)  << convert(dollar, rate.peso) << '\n' <<
+        setw(9) << "   CAD "       << "|"  << "    "  << setw(6) << rate.cad   << " |"  << setw(5) << "\t " << "$ "<<  fixed << setprecision(2)  << convert(dollar, rate.cad)  << '\n' <<
+        setw(9) << "   AUD "       << "|"  << "    "  << setw(6) << rate.aud   << " |"  << setw(5) << "\t " << "$ "<<  fixed << setprecision(2)  << convert(dollar, rate.aud)  << '\n' <<
+        setw(9) << "   JPY "       << "|"  << "    "  << setw(6) << rate.jpy   << " |"  << setw(5) << "\t " << "¥ "<<  fixed << setprecision(2)  << convert(dollar, rate.jpy)  << '\n' <<
+        setw(9) << "   INR "       << "|"  << "    "  << setw(6) << rate.inr   << " |"  << setw(5) << "\t " << "₹ "<<  fixed << setprecision(2)  << convert(dollar, rate.inr)  << '\n' <<
+        setw(9) << "   SGD "       << "|"  << "    "  << setw(6) << rate.sgd   << " |"  << setw(5) << "\t " << "$ "<<  fixed << setprecision(2)  << convert(dollar, rate.sgd)  << '\n' <<
+	    setw(9) << "   CHF "       << "|"  << "    "  << setw(6) << rate.chf   << " |"  << setw(5) << "\t " << "₣ "<<  fixed << setprecision(2)  << convert(dollar, rate.chf)  << '\n' <<
+	    setw(9) << "   CNY "       << "|"  << "    "  << setw(6) << rate.cny   << " |"  << setw(5) << "\t " << "¥ "<<  fixed << setprecision(2)  << convert(dollar, rate.cny)  << '\n';
+    cout << "----------------------------------------------------" << endl; 
     return 0;
 
 
     /*TODO: 
             make user able to convert btw other currency's i.e AUD to CAD
             other minor changes in code efficiently
+
     */
 
 }
--- currencyRates.cpp
+++ currencyRates.cpp
@@ -6,15 +6,16 @@ using namespace std;
 class Rates
 {
 public:
-    float usd = 1.0;
-    float euro = 0.94;
-    float peso = 18.36;
-    float gbp = 0.82;
-    float cad = 1.35;
-    float aud = 1.46;
-    float jpy = 134.75;
-    float inr = 82.74;
-    float sgd = 1.33;
-    float chf = 0.92;
-    float cny = 6.88;
+    double usd = 1.0;
+    double euro = 0.94;
+    double peso = 18.36;
+    double gbp = 0.82;
+    double cad = 1.35;
+    double aud = 1.46;
+    double jpy = 134.75;
+    double inr = 82.74;
+    double sgd = 1.33;
+    double chf = 0.92;
+    double cny = 6.88;
+    
 };
